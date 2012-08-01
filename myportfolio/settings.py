cf488fad8a816af8cb69f77f8621d77b3cf78a6e
Jonathan Chen <jonathanschen@gmail.com>
2012-08-01 18:28:12 -0400

    do this

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -1,7 +1,7 @@
 import dj_database_url
 import os.path
 
-PROJECT_DIR = os.path.dirname(__file__) 
+PROJECT_DIR = os.path.dirname(__file__)
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -56,7 +56,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/Users/jonathanschen/Python/projects/skeleton/myportfolio/jonathanschen/static'
+STATIC_ROOT = 'Users/jonathanschen/Python/projects/skeleton/myportfolio/jonathanschen/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR, "templates"),
+    '/Users/jonathanschen/Python/projects/skeleton/myportfolio/templates',
 )
 
 INSTALLED_APPS = (
